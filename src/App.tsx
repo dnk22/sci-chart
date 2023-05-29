70e96b6f746c0deb08ef13c827ea4a3e2c4b5c08
Duy Nguyen <duynk198@gmail.com>
2023-05-29 23:39:13 +0700

    update realtime

--- src/App.tsx
+++ src/App.tsx
@@ -1,335 +1,51 @@
 import * as React from "react";
-import {
-  SciChartSurface,
-  NumberRange,
-  NumericAxis,
-  OhlcDataSeries,
-  FastCandlestickRenderableSeries,
-  ZoomPanModifier,
-  ZoomExtentsModifier,
-  MouseWheelZoomModifier,
-  ENumericFormat,
-  DateTimeNumericAxis,
-  EAutoRange,
-  FastLineRenderableSeries,
-  XyMovingAverageFilter,
-  IDeletable,
-  SciChartOverview,
-  CursorModifier,
-  CursorTooltipSvgAnnotation,
-  SeriesInfo,
-  EDataSeriesType,
-  ESeriesType,
-  IRenderableSeries,
-  FastMountainRenderableSeries,
-  GradientParams,
-  Point,
-  OhlcSeriesInfo,
-  FastColumnRenderableSeries,
-  XyDataSeries,
-  EFillPaletteMode,
-  IFillPaletteProvider,
-  IPointMetadata,
-  parseColorToUIntArgb,
-  FastOhlcRenderableSeries,
-} from "scichart";
+import { IDeletable } from "scichart";
 import { ToggleButton, ToggleButtonGroup } from "@material-ui/lab";
 import {
   appTheme,
   simpleBinanceRestClient,
+  TPriceBar,
 } from "scichart-example-dependencies";
 import classes from "./styles/Examples.module.scss";
-import { getChartDataFromAPI } from "./api";
+import { createCandlestickChart } from "./createCandlestickChart";
+import { binanceSocketClient } from "./binanceSocketClient";
+import { Subscription } from "rxjs";
 
 const divElementId = "chart";
 const divOverviewId = "overview";
-const Y_AXIS_VOLUME_ID = "Y_AXIS_VOLUME_ID";
-const time = 1; // minutes
+const time = 1; // minute
 
-// SCICHART EXAMPLE
 const drawExample = async () => {
-  // Create a SciChartSurface
-  const { sciChartSurface, wasmContext } = await SciChartSurface.create(
-    divElementId,
-    {
-      theme: appTheme.SciChartJsTheme,
-    }
-  );
-  SciChartSurface.setRuntimeLicenseKey(
-    "v0cMj2E7YzsoEOW7S9Q3pn1R9z3NbWJLjksh/Ii0r7+iPNugTcbY0ii2irkxTEzajCYcMWO3iDBQHBWIurLvDFpVIjfTGwh2hFkgY38af+h1lZME/OArdfgAQejOcLJAX+yc1xftI9VhKPEpMNudDHnFefIsKYogvhCPM1M8Vuxz26wqb6InKU9LW8DbiBvMWGE2984vmX5DIidh6ZF4d3uf3IuHTUj2Q2CoTntvhmIUGlZaKmbtf6RVTpBrYwbFJFrHXtWE8JIVGy/qyMgBrq/oe+YuA8pxwKoxNEWSwyfks9lK7v3R7wZPmSV7Gv6p9bncmYsC+cIKZokj3gGjhiMXXlqH6Vz2cre9rEtC/LWyg4dJs+tCdcipOxAFq5mYi3c56LevOH7Lgtlc1tWrm642XTbWqay7OUMuUobIJ1dTsh3g6bWl6MbJObjtCaCcXiDmuVojdVCn76JjytZlXJwdsh1bmv7uxSsf07pFEWk60nF/weIv1NKf34cDu30NhNGuZtzaOeDxxE3QFtXwmBGc3HR2ntlblEagrdj79q/fz7I4WL+OPdsKEIoDiyQ="
-  );
+  // Create the candlestick chart example. Contains Candlestick series, tooltips, volume, zooming panning behaviour and more
+  const { sciChartSurface, sciChartOverview, controls } =
+    await createCandlestickChart(divElementId, divOverviewId);
 
-  // Add an XAxis of type DateTimeAxis
-  // Note for crypto data this is fine, but for stocks/forex you will need to use CategoryAxis which collapses gaps at weekends
-  // In future we have a hybrid IndexDateAxis which 'magically' solves problems of different # of points in stock market datasetd with gaps
-  const xAxis = new DateTimeNumericAxis(wasmContext, {
-    // autoRange.never as we're setting visibleRange explicitly below. If you dont do this, leave this flag default
-    autoRange: EAutoRange.Never,
-  });
-  sciChartSurface.xAxes.add(xAxis);
-
-  // Create a NumericAxis on the YAxis with 2 Decimal Places
-  sciChartSurface.yAxes.add(
-    new NumericAxis(wasmContext, {
-      growBy: new NumberRange(0.000001, 0.000001),
-      labelFormat: ENumericFormat.Decimal,
-      labelPrecision: 9,
-      labelPrefix: "$",
-      autoRange: EAutoRange.Always,
-    })
-  );
-
-  // Create a secondary YAxis to host volume data on its own scale
-  sciChartSurface.yAxes.add(
-    new NumericAxis(wasmContext, {
-      id: Y_AXIS_VOLUME_ID,
-      growBy: new NumberRange(0, 4),
-      isVisible: false,
-      autoRange: EAutoRange.Always,
-    })
-  );
-
-  // Fetch data from now to 300 1hr candles ago
   const endDate = new Date(Date.now());
   const startDate = new Date();
-  startDate.setHours(endDate.getHours() - 300);
+  startDate.setMinutes(endDate.getMinutes() - 300);
 
-  const priceBars = await getChartDataFromAPI();
+  // Fetch data Binance exchange: 300 1-minute candles
+  const priceBars = await simpleBinanceRestClient.getCandles(
+    "BTCUSDT",
+    "1m",
+    startDate,
+    endDate
+  );
 
-  // Maps PriceBar { date, open, high, low, close, volume } to structure-of-arrays expected by scichart
-  const xValues: number[] = [];
-  const openValues: number[] = [];
-  const highValues: number[] = [];
-  const lowValues: number[] = [];
-  const closeValues: number[] = [];
-  const volumeValues: number[] = [];
-  priceBars.forEach((priceBar: any) => {
-    const keys = Object.keys(priceBar);
-    const firstKey: any = keys[0];
-    xValues.push(+firstKey / 1000);
-    openValues.push(priceBar[firstKey][0]);
-    highValues.push(priceBar[firstKey][1]);
-    lowValues.push(priceBar[firstKey][2]);
-    closeValues.push(priceBar[firstKey][3]);
-    volumeValues.push(
-      +priceBar[firstKey][0] +
-        priceBar[firstKey][1] +
-        priceBar[firstKey][2] +
-        priceBar[firstKey][3]
-    );
-  });
+  // Set the candles data on the chart
+  controls.setData("M5", "5 Minute", priceBars);
 
   // Zoom to the latest 100 candles
   const startViewportRange = new Date();
-  startViewportRange.setHours(startDate.getHours() - 100);
-  xAxis.visibleRange = new NumberRange(
-    startViewportRange.getTime() / 1000,
-    endDate.getTime() / 1000
-  );
+  startViewportRange.setMinutes(endDate.getMinutes() - 100);
+  endDate.setMinutes(endDate.getMinutes() + 10);
+  controls.setXRange(startViewportRange, endDate);
 
-  // Create and add the Candlestick series
-  // The Candlestick Series requires a special dataseries type called OhlcDataSeries with o,h,l,c and date values
-  const candleDataSeries = new OhlcDataSeries(wasmContext, {
-    xValues,
-    openValues,
-    highValues,
-    lowValues,
-    closeValues,
-    dataSeriesName: "BTC/USDT",
-  });
-  const candlestickSeries = new FastCandlestickRenderableSeries(wasmContext, {
-    dataSeries: candleDataSeries,
-    stroke: appTheme.ForegroundColor, // used by cursorModifier below
-    strokeThickness: 1,
-    brushUp: appTheme.VividGreen + "77",
-    brushDown: appTheme.MutedRed + "77",
-    strokeUp: appTheme.VividGreen,
-    strokeDown: appTheme.MutedRed,
-  });
-  sciChartSurface.renderableSeries.add(candlestickSeries);
-
-  // Add an Ohlcseries. this will be invisible to begin with
-  const ohlcSeries = new FastOhlcRenderableSeries(wasmContext, {
-    dataSeries: candleDataSeries,
-    stroke: appTheme.ForegroundColor, // used by cursorModifier below
-    strokeThickness: 1,
-    dataPointWidth: 0.9,
-    strokeUp: appTheme.VividGreen,
-    strokeDown: appTheme.MutedRed,
-    isVisible: false,
-  });
-  sciChartSurface.renderableSeries.add(ohlcSeries);
-
-  // Add some moving averages using SciChart's filters/transforms API
-  // when candleDataSeries updates, XyMovingAverageFilter automatically recomputes
-  sciChartSurface.renderableSeries.add(
-    new FastLineRenderableSeries(wasmContext, {
-      dataSeries: new XyMovingAverageFilter(candleDataSeries, {
-        dataSeriesName: "Moving Average (20)",
-        length: 20,
-      }),
-      stroke: appTheme.VividSkyBlue,
-    })
-  );
-
-  sciChartSurface.renderableSeries.add(
-    new FastLineRenderableSeries(wasmContext, {
-      dataSeries: new XyMovingAverageFilter(candleDataSeries, {
-        dataSeriesName: "Moving Average (50)",
-        length: 50,
-      }),
-      stroke: appTheme.VividPink,
-    })
-  );
-
-  // Add volume data onto the chart
-  sciChartSurface.renderableSeries.add(
-    new FastColumnRenderableSeries(wasmContext, {
-      dataSeries: new XyDataSeries(wasmContext, {
-        xValues,
-        yValues: volumeValues,
-        dataSeriesName: "Volume",
-      }),
-      strokeThickness: 0,
-      // This is how we get volume to scale - on a hidden YAxis
-      yAxisId: Y_AXIS_VOLUME_ID,
-      // This is how we colour volume bars red or green
-      paletteProvider: new VolumePaletteProvider(
-        candleDataSeries,
-        appTheme.VividGreen + "77",
-        appTheme.MutedRed + "77"
-      ),
-    })
-  );
-
-  // Optional: Add some interactivity modifiers
-  sciChartSurface.chartModifiers.add(
-    new ZoomExtentsModifier(),
-    new ZoomPanModifier(),
-    new MouseWheelZoomModifier(),
-    new CursorModifier({
-      crosshairStroke: appTheme.VividOrange,
-      axisLabelFill: appTheme.VividOrange,
-      tooltipLegendTemplate: getTooltipLegendTemplate,
-    })
-  );
-
-  // Add Overview chart. This will automatically bind to the parent surface
-  // displaying its series. Zooming the chart will zoom the overview and vice versa
-  const overview = await SciChartOverview.create(
-    sciChartSurface,
-    divOverviewId,
-    {
-      theme: appTheme.SciChartJsTheme,
-      transformRenderableSeries: getOverviewSeries,
-    }
-  );
-
-  return { sciChartSurface, overview, candlestickSeries, ohlcSeries };
-};
-
-// Override the Renderableseries to display on the scichart overview
-const getOverviewSeries = (defaultSeries: IRenderableSeries): any => {
-  if (defaultSeries.type === ESeriesType.CandlestickSeries) {
-    // Swap the default candlestick series on the overview chart for a mountain series. Same data
-    return new FastMountainRenderableSeries(
-      defaultSeries.parentSurface.webAssemblyContext2D,
-      {
-        dataSeries: defaultSeries.dataSeries,
-        fillLinearGradient: new GradientParams(
-          new Point(0, 0),
-          new Point(0, 1),
-          [
-            { color: appTheme.VividSkyBlue + "77", offset: 0 },
-            { color: "Transparent", offset: 1 },
-          ]
-        ),
-        stroke: appTheme.VividSkyBlue,
-      }
-    );
-  }
-  // hide all other series
-  return undefined;
-};
-
-// Override the standard tooltip displayed by CursorModifier
-const getTooltipLegendTemplate = (
-  seriesInfos: SeriesInfo[],
-  svgAnnotation: CursorTooltipSvgAnnotation
-) => {
-  let outputSvgString = "";
-
-  // Foreach series there will be a seriesInfo supplied by SciChart. This contains info about the series under the house
-  seriesInfos.forEach((seriesInfo, index) => {
-    const y = 20 + index * 20;
-    const textColor = seriesInfo.stroke;
-    let legendText = seriesInfo.formattedYValue;
-    if (seriesInfo.dataSeriesType === EDataSeriesType.Ohlc) {
-      const o = seriesInfo as OhlcSeriesInfo;
-      legendText = `Open=${o.formattedOpenValue} High=${o.formattedHighValue} Low=${o.formattedLowValue} Close=${o.formattedCloseValue}`;
-    }
-    outputSvgString += `<text x="8" y="${y}" font-size="13" font-family="Verdana" fill="${textColor}">
-            ${seriesInfo.seriesName}: ${legendText}
-        </text>`;
-  });
-
-  return `<svg width="100%" height="100%">
-                ${outputSvgString}
-            </svg>`;
-};
-
-class VolumePaletteProvider implements IFillPaletteProvider {
-  fillPaletteMode: EFillPaletteMode = EFillPaletteMode.SOLID;
-  private ohlcDataSeries: OhlcDataSeries;
-  private upColorArgb: number;
-  private downColorArgb: number;
-
-  constructor(masterData: OhlcDataSeries, upColor: string, downColor: string) {
-    this.upColorArgb = parseColorToUIntArgb(upColor);
-    this.downColorArgb = parseColorToUIntArgb(downColor);
-    this.ohlcDataSeries = masterData;
-  }
-  onAttached(parentSeries: IRenderableSeries): void {}
-  onDetached(): void {}
-
-  // Return up or down color for the volume bars depending on Ohlc data
-  overrideFillArgb(
-    xValue: number,
-    yValue: number,
-    index: number,
-    opacity?: number,
-    metadata?: IPointMetadata
-  ): number {
-    const isUpCandle =
-      this.ohlcDataSeries.getNativeOpenValues().get(index) >=
-      this.ohlcDataSeries.getNativeCloseValues().get(index);
-    return isUpCandle ? this.upColorArgb : this.downColorArgb;
-  }
-}
-
-// React component needed as our examples app is react.
-// SciChart can be used in Angular, Vue, Blazor and vanilla JS! See our Github repo for more info
-export default function CandlestickChart() {
-  const itemsToDelete: IDeletable[] = [];
-  const [preset, setPreset] = React.useState<number>(0);
-  const [candlestickChartSeries, setCandlestickChartSeries] = React.useState<
-    FastCandlestickRenderableSeries | any
-  >();
-  const [ohlcChartSeries, setOhlcChartSeries] = React.useState<
-    FastOhlcRenderableSeries | any
-  >();
-
-  async function renderChart() {
-    const { sciChartSurface, overview, candlestickSeries, ohlcSeries } =
-      await drawExample();
-    setCandlestickChartSeries(candlestickSeries);
-    setOhlcChartSeries(ohlcSeries);
-    itemsToDelete.push(sciChartSurface, overview);
-  }
+  async function updateChart() {}
 
   // Function to fetch data and push to array, called every 5 minutes
   async function fetchDataAndUpdateArray() {
-    return renderChart()
+    return updateChart()
       .then(() => {
         // Schedule the next API call after x minutes
         setTimeout(fetchDataAndUpdateArray, time * 60 * 1000);
@@ -342,21 +58,66 @@ export default function CandlestickChart() {
         return false;
       });
   }
+  // Susbscribe to price updates from the exchange
+  const subscription = binanceSocketClient
+    .getRealtimeCandleStream("BTCUSDT", "1m")
+    .subscribe((pb) => {
+      const priceBar = {
+        date: pb.openTime,
+        open: pb.open,
+        high: pb.high,
+        low: pb.low,
+        close: pb.close,
+        volume: pb.volume,
+      };
+      controls.onNewTrade(priceBar, pb.lastTradeSize, pb.lastTradeBuyOrSell);
+    });
+
+  return { sciChartSurface, sciChartOverview, subscription, controls };
+};
+
+// React component needed as our examples app is react.
+// SciChart can be used in Angular, Vue, Blazor and vanilla JS! See our Github repo for more info
+export default function RealtimeTickingStockCharts() {
+  const itemsToDelete: IDeletable[] = [];
+  let websocketSubcription: Subscription;
+  const [preset, setPreset] = React.useState<number>(0);
+  const [chartControls, setControls] = React.useState({
+    setData: (
+      symbolName: string,
+      watermarkText: string,
+      priceBars: TPriceBar[]
+    ) => {},
+    onNewTrade: (
+      priceBar: TPriceBar,
+      tradeSize: number,
+      lastTradeBuyOrSell: boolean
+    ) => {},
+    setXRange: (startDate: Date, endDate: Date) => {},
+    enableCandlestick: () => {},
+    enableOhlc: () => {},
+  });
 
   React.useEffect(() => {
-    fetchDataAndUpdateArray();
+    (async () => {
+      const { sciChartSurface, sciChartOverview, subscription, controls } =
+        await drawExample();
+      setControls(controls);
+      websocketSubcription = subscription;
+      itemsToDelete.push(sciChartSurface, sciChartOverview);
+    })();
     return () => {
       itemsToDelete.forEach((item) => item.delete());
+      websocketSubcription?.unsubscribe();
     };
   }, []);
 
   const handleToggleButtonChanged = (event: any, state: number) => {
-    if (state === null) return;
+    if (state === null || chartControls === undefined) return;
     setPreset(state);
     console.log(`Toggling Candle/Ohlc state: ${state}`);
-    // Toggle visibility of candlestick or OHLC series
-    candlestickChartSeries.isVisible = state === 0;
-    ohlcChartSeries.isVisible = state === 1;
+    if (state === 0) chartControls.enableCandlestick();
+    if (state === 1) chartControls.enableOhlc();
   };
 
   return (
